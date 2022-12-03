5b2e8dbbd975aec2c3a1e2f968ea409e1ac46ffc
Prizrakost <34614040+Prizrakost@users.noreply.github.com>
2022-12-04 01:32:31 +1000

    Update Network.ino

--- Network.ino
+++ Network.ino
@@ -11,7 +11,7 @@ void setupAP() {
   IPAddress gateway(192,168,1,1);
   IPAddress subnet(255,255,255,0);
   WiFi.softAPConfig(local_ip, gateway, subnet);
-  WiFi.softAP(cfg.APssid, cfg.APpassword);
+  WiFi.softAP(APssid, APpassword);
   delay(500);
 }
 
