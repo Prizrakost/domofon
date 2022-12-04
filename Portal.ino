88992e8323e9ef9562b1cc9463a9e521692d09a3
Prizrakost <34614040+Prizrakost@users.noreply.github.com>
2022-12-04 21:50:11 +1000

    Update Portal.ino

--- Portal.ino
+++ Portal.ino
@@ -100,7 +100,7 @@ void action() {
 
       // Изменить доступ, где надо
       // (надеюсь. так сработает)
-      // Попользовать “Массив” компонентов file:///C:/Users/remem/myThings/pandoc-2.19.2/GyverPortal.wiki/%D0%9E%D0%B1%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%BA%D0%B0-%D0%BA%D0%BB%D0%B8%D0%BA%D0%BE%D0%B2.html
+      // Попользовать “Массив” компонентов? file:///C:/Users/remem/myThings/pandoc-2.19.2/GyverPortal.wiki/%D0%9E%D0%B1%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%BA%D0%B0-%D0%BA%D0%BB%D0%B8%D0%BA%D0%BE%D0%B2.html
       // Попользовать кастомную хрень?
       if (portal.clickSub("access#")) {
         Serial.println(portal.clickName()); // возвращает access#номер ключа
